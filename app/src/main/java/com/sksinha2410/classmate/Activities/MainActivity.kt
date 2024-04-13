ea9be8f0afb49796a05d62f5bfcc9df8f159303d
Shivam Kumar Sinha <sksinha200276@gmail.com>
2024-04-13 15:17:25 +0530

    Created Fragments

--- app/src/main/java/com/sksinha2410/classmate/Activities/MainActivity.kt
+++ app/src/main/java/com/sksinha2410/classmate/Activities/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.sksinha2410.classmate
+package com.sksinha2410.classmate.Activities
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.sksinha2410.classmate.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
